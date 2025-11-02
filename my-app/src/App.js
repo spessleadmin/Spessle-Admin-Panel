d0f487ebbea2110d0ef199d96537060ac64c94c6
Cubed <39543078+Cubed@users.noreply.github.com>
2025-11-01 21:33:13 -0400

    manage business

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,6 +5,7 @@ import TestPage from "./TestPage";
 import Dashboard from "./Dashboard";
 import AdminUsers from "./AdminUsers";
 import ManageProduct from "./ManageProduct";
+import ManageBusiness from "./ManageBusiness";
 import EditProduct from "./EditProduct";
 import SamplePage from "./SamplePage";
 
@@ -18,6 +19,7 @@ function App() {
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/admin-users" element={<AdminUsers />} />
         <Route path="/manage-product" element={<ManageProduct />} />
+        <Route path="/manage-business" element={<ManageBusiness />} />
         <Route path="/edit-product/:id" element={<EditProduct />} />
         <Route path="/sample-page" element={<SamplePage />} />
       </Routes>
